67b009035fe0be5934d87fd726f334728955a739
Nik Hildebrandt <nik.hildebrandt@proton.me>
2025-12-10 21:30:16 +0100

    Added conan detection to BuildType switch

--- lua/plugins/cmake-tools.lua
+++ lua/plugins/cmake-tools.lua
@@ -1,80 +1,87 @@
 if not Functionalities.cpp then
-    return {}
+  return {}
 end
 
 
 return {
-    'Civitasv/cmake-tools.nvim',
-    lazy = true,
-    ft = { 'c', 'cpp', 'h', 'hpp', 'cmake' },
-    opts = function()
-        local opts = {
-            cmake_build_options = { '--parallel' },
-            cmake_generate_options = { "-GNinja", "-DCMAKE_EXPORT_COMPILE_COMMANDS=1" },
-            cmake_regenerate_on_save = false,
-            cmake_executor = { name = 'toggleterm' },
-            cmake_runner = { name = 'toggleterm' },
-        }
+  'Civitasv/cmake-tools.nvim',
+  lazy = true,
+  ft = { 'c', 'cpp', 'h', 'hpp', 'cmake' },
+  opts = function()
+    local opts = {
+      cmake_build_options = { '--parallel' },
+      cmake_generate_options = { "-GNinja", "-DCMAKE_EXPORT_COMPILE_COMMANDS=1" },
+      cmake_regenerate_on_save = false,
+      cmake_executor = { name = 'toggleterm' },
+      cmake_runner = { name = 'toggleterm' },
+    }
 
-        -- Windows cant support the default softlink option so copy is used
-        if IS_WINDOWS then
-            opts.cmake_compile_commands_options = { action = "copy" }
-        end
+    -- Windows cant support the default softlink option so copy is used
+    if IS_WINDOWS then
+      opts.cmake_compile_commands_options = { action = "copy" }
+    end
 
-        return opts
-    end,
-    keys = {
-        {
-            "<leader>cB",
-            function()
-                vim.cmd("CMakeSelectBuildTarget")
-            end,
-            desc = "Select CMakeBuild Target",
-        },
-        {
-            "<leader>cb",
-            function()
-                vim.cmd("CMakeBuild")
-            end,
-            desc = "CMakeBuild",
-        },
-        {
-            "<leader>cg",
-            function()
-                vim.cmd("CMakeGenerate")
-            end,
-            desc = "CMakeGenerate",
-        },
-        {
-            "<leader>cR",
-            function()
-                vim.cmd("CMakeSelectLaunchTarget")
-            end,
-            desc = "Select CMakeRun Target",
-        },
-        {
-            "<leader>cr",
-            function()
-                vim.cmd("CMakeRun")
-            end,
-            desc = "CMakeRun",
-        },
-        {
-            "<leader>cs",
-            function()
-                vim.cmd("CMakeSelectBuildType")
-            end,
-            desc = "CMakeSelectBuildType",
-
-        },
-        {
-            "<leader>ct",
-            function()
-                vim.cmd("CMakeRunTest")
-            end,
-            desc = "CMakeRunTest",
-        },
+    return opts
+  end,
+  keys = {
+    {
+      "<leader>cB",
+      function()
+        vim.cmd("CMakeSelectBuildTarget")
+      end,
+      desc = "Select CMakeBuild Target",
     },
+    {
+      "<leader>cb",
+      function()
+        vim.cmd("CMakeBuild")
+      end,
+      desc = "CMakeBuild",
+    },
+    {
+      "<leader>cg",
+      function()
+        vim.cmd("CMakeGenerate")
+      end,
+      desc = "CMakeGenerate",
+    },
+    {
+      "<leader>cR",
+      function()
+        vim.cmd("CMakeSelectLaunchTarget")
+      end,
+      desc = "Select CMakeRun Target",
+    },
+    {
+      "<leader>cr",
+      function()
+        vim.cmd("CMakeRun")
+      end,
+      desc = "CMakeRun",
+    },
+    {
+      "<leader>cs",
+      function()
+        local cwd = vim.fn.getcwd()
+        local files = vim.fn.glob(cwd .. "/conanfile.*", false, true)
+
+        if next(files) ~= nil then
+          vim.cmd("CMakeSelectConfigurePreset")
+        else
+          vim.cmd("CMakeSelectBuildType")
+        end
+      end,
+      desc = "Select BuildType with conan detection",
+
+    },
+    {
+      "<leader>ct",
+      function()
+        vim.cmd("CMakeRunTest")
+      end,
+      desc = "CMakeRunTest",
+    },
+  },
 }
 
 --[[ example kit from my homePC
