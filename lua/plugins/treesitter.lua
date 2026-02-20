683854b4135a58f88b015ba915d2f5fe70204eb5
Nik Hildebrandt <nik.hildebrandt@proton.me>
2026-02-20 21:46:31 +0100

    changed to comply with treesitter naming change

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ local M = {
     require('nvim-treesitter.install').update({ with_sync = true })()
   end,
   config = function()
-    local configs = require('nvim-treesitter.configs')
+    local configs = require('nvim-treesitter.config')
     local list = { 'lua', 'markdown' }
     if Functionalities.cpp then
       table.insert(list, 'c')
