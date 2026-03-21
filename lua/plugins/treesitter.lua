1477a4dee0663addff8a106ce7247eb5d0391d0b
Nik Hildebrandt <nik.hildebrandt@proton.me>
2026-03-21 12:26:18 +0100

    Modified treesitter to match breaking changes in main branch

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,9 @@
 local M = {
   'nvim-treesitter/nvim-treesitter',
-  build = function()
-    require('nvim-treesitter.install').update({ with_sync = true })
-  end,
+  build = ":TSUpdate",
+  lazy = false,
   config = function()
-    local configs = require('nvim-treesitter.config')
+    local ts = require('nvim-treesitter')
     local list = { 'lua', 'markdown' }
     if Functionalities.cpp then
       table.insert(list, 'c')
@@ -21,13 +20,10 @@ local M = {
       table.insert(list, 'rust')
     end
 
-    configs.setup({
-      ensure_installed = list,
-      ignore_install = { 'latex' },
-      sync_install = false,
-      auto_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
+    ts.install(list)
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = list,
+      callback = function() vim.treesitter.start() end,
     })
   end,
 }
