1b2ba4883448ef2625e8ec3324efcc7d0140cc9f
Nik Hildebrandt <nik.hildebrandt@proton.me>
2026-04-20 08:14:08 +0200

    Tried setting typst file specific settings with ftplugin

---
+++ after/ftplugin/typst.lua
@@ -0,0 +1,12 @@
+-- Enable spell checking
+vim.opt_local.spell = true
+
+-- Set language to german
+vim.opt_local.spelllang = "de"
+
+-- Set textwith to 80
+vim.opt_local.textwidth = 80
+
+-- Enable automatic wrapping
+vim.opt_local.wrap = true
+vim.opt_local.linebreak = true
